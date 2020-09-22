c31010539db4bc0df20c8d01a6a3ee0544a9e750
lamafab <42901763+lamafab@users.noreply.github.com>
2020-09-22 16:15:13 +0200

    create signature for twitter api

--- src/adapters/twitter.rs
+++ src/adapters/twitter.rs
@@ -12,8 +12,10 @@ pub enum TwitterError {
 
 pub struct TwitterBuilder {
     consumer_key: Option<String>,
+    consumer_secret: Option<String>,
     sig_method: Option<String>,
     token: Option<String>,
+    token_secret: Option<String>,
     version: Option<f64>,
 }
 
@@ -21,13 +23,19 @@ impl TwitterBuilder {
     pub fn new() -> Self {
         TwitterBuilder {
             consumer_key: None,
+            consumer_secret: None,
             sig_method: None,
             token: None,
+            token_secret: None,
             version: None,
         }
     }
     pub fn consumer_key(mut self, key: String) -> Self {
         self.consumer_key = Some(key);
+        self
+    }
+    pub fn consumer_secret(mut self, key: String) -> Self {
+        self.consumer_secret = Some(key);
         self
     }
     pub fn sig_method(mut self, method: String) -> Self {
@@ -38,6 +46,10 @@ impl TwitterBuilder {
         self.token = Some(token);
         self
     }
+    pub fn token_secret(mut self, secret: String) -> Self {
+        self.token_secret = Some(secret);
+        self
+    }
     pub fn version(mut self, version: f64) -> Self {
         self.version = Some(version);
         self
@@ -46,8 +58,12 @@ impl TwitterBuilder {
         Ok(Twitter {
             client: Client::new(),
             consumer_key: self.consumer_key.ok_or(TwitterError::IncompleteBuilder)?,
+            consumer_secret: self
+                .consumer_secret
+                .ok_or(TwitterError::IncompleteBuilder)?,
             sig_method: self.sig_method.ok_or(TwitterError::IncompleteBuilder)?,
             token: self.token.ok_or(TwitterError::IncompleteBuilder)?,
+            token_secret: self.token_secret.ok_or(TwitterError::IncompleteBuilder)?,
             version: self.version.ok_or(TwitterError::IncompleteBuilder)?,
         })
     }
@@ -56,28 +72,35 @@ impl TwitterBuilder {
 pub struct Twitter {
     client: Client,
     consumer_key: String,
+    consumer_secret: String,
     sig_method: String,
     token: String,
+    token_secret: String,
     version: f64,
 }
 
 use hmac::{Hmac, Mac, NewMac};
 use sha1::Sha1;
 
-fn sign(mut request: Request) {
-    let mut headers: Vec<(&HeaderName, &HeaderValue)> = request
-        .headers_mut()
-        .iter()
-        .collect::<Vec<(&HeaderName, &HeaderValue)>>();
-    headers.sort_by(|(a, _), (b, _)| a.as_str().cmp(b.as_str()));
+enum HttpMethod {
+    POST,
+    GET,
+}
 
-    let mut mac: Hmac<Sha1> = Hmac::new_varkey(b"").unwrap();
-    mac.update(b"");
+impl HttpMethod {
+    fn as_str(&self) -> &'static str {
+        use HttpMethod::*;
+
+        match self {
+            POST => "POST",
+            GET => "GET",
+        }
+    }
 }
 
 impl Twitter {
-    pub async fn request<T: DeserializeOwned>(&self, url: &str) -> Result<T> {
-        let request = self
+    fn create_request(&self, method: HttpMethod, url: &str) -> Result<Request> {
+        Ok(self
             .client
             .get(url)
             .header(header::AUTHORIZATION, HeaderValue::from_str("OAuth")?)
@@ -96,10 +119,6 @@ impl Twitter {
                 HeaderValue::from_str(Challenge::gen_random().as_str())?,
             )
             .header(
-                HeaderName::from_static("oauth_signature"),
-                HeaderValue::from_str("")?,
-            )
-            .header(
                 HeaderName::from_static("oauth_signature_method"),
                 HeaderValue::from_str(&self.sig_method)?,
             )
@@ -115,10 +134,59 @@ impl Twitter {
                 HeaderName::from_static("oauth_version"),
                 HeaderValue::from_str(&self.version.to_string())?,
             )
-            .build()?;
-
-        Ok(self.client.execute(request).await?.json::<T>().await?)
+            .build()?)
     }
+    /// Creates a signature as documented here:
+    /// https://developer.twitter.com/en/docs/authentication/oauth-1-0a/creating-a-signature
+    fn create_sig(
+        &self,
+        method: HttpMethod,
+        url: &str,
+        request: &Request,
+        body: &[(&str, &str)],
+    ) -> Result<String> {
+        use urlencoding::encode;
+
+        let mut fields: Vec<(&str, &str)> = request
+            .headers()
+            .iter()
+            .map(|(k, v)| (k.as_str(), v.to_str().unwrap()))
+            .chain(body.iter().map(|&s| s))
+            .collect();
+
+        fields.sort_by(|(a, _), (b, _)| a.cmp(b));
+
+        let key = format!("{}&{}", self.consumer_secret, self.token_secret);
+        let mut mac: Hmac<Sha1> = Hmac::new_varkey(key.as_bytes()).unwrap();
+
+        mac.update(method.as_str().as_bytes());
+        mac.update(encode("&").as_bytes());
+        mac.update(encode(url).as_bytes());
+        mac.update(encode("&").as_bytes());
+
+        let mut first = true;
+        for (name, val) in fields {
+            if !first {
+                mac.update(encode("&").as_bytes());
+            } else {
+                first = false;
+            }
+
+            mac.update(encode(name).as_bytes());
+            mac.update(encode("=").as_bytes());
+            mac.update(encode(val).as_bytes());
+        }
+
+        let sig = base64::encode(mac.finalize().into_bytes());
+        Ok(sig)
+    }
+    /*
+    pub async fn get_request<T: DeserializeOwned>(&self, url: &str) -> Result<T> {
+
+        panic!()
+        //Ok(self.client.execute(request).await?.json::<T>().await?)
+    }
+    */
     pub async fn start(self) {
         let mut interval = time::interval(Duration::from_secs(3));
         loop {
@@ -133,3 +201,6 @@ impl Twitter {
         Ok(())
     }
 }
+
+#[test]
+fn test_twitter() {}
