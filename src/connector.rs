be44295ac62947e322f31a6cb0ba6209f0601f31
lamafab <42901763+lamafab@users.noreply.github.com>
2020-09-30 20:43:27 +0200

    formatting

--- src/connector.rs
+++ src/connector.rs
@@ -1,20 +1,20 @@
 use crate::comms::{CommsMessage, CommsVerifier};
 use crate::identity::OnChainIdentity;
 use crate::primitives::{Account, AccountType, Judgement, NetAccount, Result};
+use futures::sink::SinkExt;
+use futures::stream::{SplitSink, SplitStream};
+use futures::StreamExt;
 use futures::{select_biased, FutureExt};
 use serde_json::Value;
 use std::collections::HashMap;
 use std::convert::TryFrom;
 use std::result::Result as StdResult;
-use websockets::{Frame, WebSocket};
+use tokio::net::TcpStream;
 use tokio::time::{self, Duration};
 use tokio_tungstenite::{connect_async, WebSocketStream};
-use tokio::net::TcpStream;
-use futures::StreamExt;
-use futures::sink::SinkExt;
-use futures::stream::{SplitStream, SplitSink};
-use tungstenite::protocol::Message as TungMessage;
 use tungstenite::error::Error as TungError;
+use tungstenite::protocol::Message as TungMessage;
+use websockets::{Frame, WebSocket};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 enum EventType {
@@ -135,7 +135,7 @@ impl Connector {
     pub async fn start(self) {
         let (client, _) = connect_async(&self.url).await.unwrap();
         let (write, read) = client.split();
- 
+
         //let c_self = self.clone();
         let comms = self.comms.clone();
         tokio::spawn(async move {
@@ -146,81 +146,93 @@ impl Connector {
             // TODO: This must be initialized outside, use Cell.
             let mut receiver_error = false;
 
-            message.map_err(|err| {
-                match err {
-                    TungError::ConnectionClosed => {
-                        // Prevent spamming log messages if the server is
-                        // disconnected.
-                        if !receiver_error {
-                            error!("Disconnected from Listener: {}", err);
-                            receiver_error = true;
+            message
+                .map_err(|err| {
+                    match err {
+                        TungError::ConnectionClosed => {
+                            // Prevent spamming log messages if the server is
+                            // disconnected.
+                            if !receiver_error {
+                                error!("Disconnected from Listener: {}", err);
+                                receiver_error = true;
+                            }
+
+                            // Try to silently reconnect
+                            // TODO...
                         }
+                        _ => {
+                            receiver_error = false;
+                            error!("{}", err)
+                        }
+                    };
+                })
+                .map(|message| {
+                    match message {
+                        TungMessage::Text(payload) => {
+                            trace!("Received message from Watcher: {}", payload);
+                            let try_msg = serde_json::from_str::<Message>(&payload);
+                            let msg = if let Ok(msg) = try_msg {
+                                msg
+                            } else {
+                                // TODO: Respond with error
+                                return;
+                            };
 
-                        // Try to silently reconnect
-                        // TODO...
-                    },
-                    _ => {
-                        receiver_error = false;
-                        error!("{}", err)
-                    }
-                };
-            }).map(|message| {
-                match message {
-                    TungMessage::Text(payload) => {
-                        trace!("Received message from Watcher: {}", payload);
-                        let try_msg = serde_json::from_str::<Message>(&payload);
-                        let msg = if let Ok(msg) = try_msg {
-                            msg
-                        } else {
-                            // TODO: Respond with error
-                            return;
-                        };
-
-                        match msg.event {
-                            EventType::NewJudgementRequest => {
-                                info!("Received a new judgement request from Watcher");
-                                if let Ok(request) = serde_json::from_value::<JudgementRequest>(msg.data) {
-                                    if let Ok(ident) = OnChainIdentity::try_from(request) {
-                                        // TODO: Respond with acknowledgement
-                                        self.comms.notify_new_identity(ident);
+                            match msg.event {
+                                EventType::NewJudgementRequest => {
+                                    info!("Received a new judgement request from Watcher");
+                                    if let Ok(request) =
+                                        serde_json::from_value::<JudgementRequest>(msg.data)
+                                    {
+                                        if let Ok(ident) = OnChainIdentity::try_from(request) {
+                                            // TODO: Respond with acknowledgement
+                                            self.comms.notify_new_identity(ident);
+                                        } else {
+                                            // TODO: Respond with error
+                                        };
                                     } else {
                                         // TODO: Respond with error
-                                    };
-                                } else {
-                                    // TODO: Respond with error
+                                    }
                                 }
+                                _ => {}
                             }
-                            _ => {}
                         }
+                        _ => {}
                     }
-                    _ => {}
-                }
-            });
+                });
         });
     }
-    async fn start_comms_receiver(comms: CommsVerifier, mut writer: SplitSink<WebSocketStream<TcpStream>, TungMessage>) {
+    async fn start_comms_receiver(
+        comms: CommsVerifier,
+        mut writer: SplitSink<WebSocketStream<TcpStream>, TungMessage>,
+    ) {
         loop {
-            let _ = Self::handle_comms_message(&comms, &mut writer).await.map_err(|err| {
-                error!("{}", err);
-            });
+            let _ = Self::handle_comms_message(&comms, &mut writer)
+                .await
+                .map_err(|err| {
+                    error!("{}", err);
+                });
         }
     }
-    async fn handle_comms_message(comms: &CommsVerifier, writer: &mut SplitSink<WebSocketStream<TcpStream>, TungMessage>) -> Result<()> {
+    async fn handle_comms_message(
+        comms: &CommsVerifier,
+        writer: &mut SplitSink<WebSocketStream<TcpStream>, TungMessage>,
+    ) -> Result<()> {
         match comms.recv().await {
             CommsMessage::JudgeIdentity {
                 net_account,
-                judgement
+                judgement,
             } => {
                 writer.send(TungMessage::Text(
-                serde_json::to_string(&Message {
-                    event: EventType::JudgementResult,
-                    data: serde_json::to_value(&JudgementResponse {
-                        address: net_account.clone(),
-                        judgement: judgement,
+                    serde_json::to_string(&Message {
+                        event: EventType::JudgementResult,
+                        data: serde_json::to_value(&JudgementResponse {
+                            address: net_account.clone(),
+                            judgement: judgement,
+                        })
+                        .map_err(|err| ConnectorError::Response(err.into()))?,
                     })
                     .map_err(|err| ConnectorError::Response(err.into()))?,
-                })
-                .map_err(|err| ConnectorError::Response(err.into()))?,
                 ));
             }
             _ => {}
