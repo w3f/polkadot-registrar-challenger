625de2319d12bcd79f50d5a65fa71f2d7828f5ee
lamafab <42901763+lamafab@users.noreply.github.com>
2020-10-01 16:37:04 +0200

    remove deprecated rocksdb database

--- src/db.rs
+++ src/db.rs
@@ -4,11 +4,9 @@ use crate::identity::{AccountStatus, OnChainIdentity};
 use crate::primitives::{
     Account, AccountType, Challenge, ChallengeStatus, NetAccount, NetworkAddress,
 };
-use failure::err_msg;
 use matrix_sdk::identifiers::RoomId;
-use rocksdb::{ColumnFamily, IteratorMode, Options, DB};
 use rusqlite::{named_params, params, Connection, OptionalExtension};
-use std::convert::{AsRef, TryFrom};
+use std::convert::TryFrom;
 use std::result::Result as StdResult;
 use std::sync::Arc;
 use tokio::sync::Mutex;
@@ -832,62 +830,6 @@ impl Database2 {
         }
 
         Ok(untracked_email_ids)
-    }
-}
-
-/// A simple abstraction layer over rocksdb. This is used primarily to have a
-/// single database object and to create `ScopedDatabase` types, in order to
-/// keep data partitioned (with column families).
-pub struct Database {
-    db: DB,
-}
-
-impl Database {
-    pub fn new(path: &str) -> Result<Self> {
-        let mut opts = Options::default();
-        opts.create_missing_column_families(true);
-        opts.create_if_missing(true);
-
-        Ok(Database {
-            //db: DB::open(&opts, path)?,
-            db: DB::open_cf(&opts, path, &["pending_identities", "matrix_rooms"])?,
-        })
-    }
-    pub fn scope<'a>(&'a self, cf_name: &str) -> ScopedDatabase<'a> {
-        ScopedDatabase {
-            db: &self.db,
-            cf_name: cf_name.to_owned(),
-        }
-    }
-}
-
-pub struct ScopedDatabase<'a> {
-    db: &'a DB,
-    // `ColumnFamily` cannot be shared between threads, so just save it as a String.
-    cf_name: String,
-}
-
-impl<'a> ScopedDatabase<'a> {
-    fn cf(&self) -> Result<&ColumnFamily> {
-        Ok(self
-            .db
-            .cf_handle(&self.cf_name)
-            .ok_or(err_msg("fatal error: column family not found"))?)
-    }
-    pub fn put<K: AsRef<[u8]>, V: AsRef<[u8]>>(&self, key: K, val: V) -> Result<()> {
-        Ok(self.db.put_cf(self.cf()?, key, val)?)
-    }
-    pub fn get<K: AsRef<[u8]>>(&self, key: K) -> Result<Option<Vec<u8>>> {
-        Ok(self.db.get_cf(self.cf()?, key)?)
-    }
-    pub fn delete<K: AsRef<[u8]>>(&self, key: K) -> Result<()> {
-        Ok(self.db.delete_cf(self.cf()?, key)?)
-    }
-    pub fn all(&self) -> Result<Vec<(Box<[u8]>, Box<[u8]>)>> {
-        Ok(self
-            .db
-            .iterator_cf(self.cf()?, IteratorMode::Start)
-            .collect())
     }
 }
 
