d02887968af18ac812a774bd7100f857084f1521
lamafab <42901763+lamafab@users.noreply.github.com>
2020-10-25 02:09:48 +0000

    test matrix_invalid_signature_response

--- src/tests/verify_matrix.rs
+++ src/tests/verify_matrix.rs
@@ -163,6 +163,7 @@ fn matrix_valid_signature_response() {
                 message: hex::encode(signature.to_bytes()),
             },
         );
+
         pause().await;
 
         // Verify events.
@@ -232,6 +233,170 @@ fn matrix_valid_signature_response() {
         }
 
         match &events[9] {
+            Event::Matrix(e) => match e {
+                MatrixEvent::LeaveRoom { room_id } => {}
+                _ => panic!(),
+            },
+            _ => panic!(),
+        }
+    });
+}
+
+#[test]
+fn matrix_invalid_signature_response() {
+    let mut rt = Runtime::new().unwrap();
+    rt.block_on(async {
+        // Setup database and manager.
+        let db = Database2::new(&db_path()).unwrap();
+        let manager = Arc::new(EventManager2::new());
+        let (_, matrix_child) = manager.child();
+
+        let my_user_id = UserId::try_from("@registrar:matrix.org").unwrap();
+        let matrix_transport = MatrixMocker::new(matrix_child, my_user_id);
+
+        // Starts tasks.
+        let handlers = test_run(
+            Arc::clone(&manager),
+            db,
+            matrix_transport,
+            DummyTransport::new(),
+            DummyTransport::new(),
+        )
+        .await
+        .unwrap();
+
+        let matrix = handlers.matrix;
+        let mut writer = handlers.writer;
+        let injector = handlers.reader.injector();
+
+        let keypair = Keypair::generate();
+
+        // Generate events.
+        let msg = serde_json::to_string(&Message {
+            event: EventType::NewJudgementRequest,
+            data: serde_json::to_value(&JudgementRequest {
+                address: NetAccount::from(&keypair.public),
+                accounts: [(
+                    AccountType::Matrix,
+                    Some(Account::from("@alice:matrix.org")),
+                )]
+                .iter()
+                .cloned()
+                .collect(),
+            })
+            .unwrap(),
+        })
+        .unwrap();
+
+        // Send new judgement request.
+        injector.send_message(msg.clone()).await;
+        pause().await;
+
+        // Respond with valid signature.
+        let invalid_signature =
+            keypair.sign_simple(b"substrate", Challenge::gen_random().as_str().as_bytes());
+
+        let valid_signature =
+            keypair.sign_simple(b"substrate", Challenge::gen_fixed().as_str().as_bytes());
+
+        matrix.trigger_matrix_emitter(
+            RoomId::try_from("!1234:matrix.org").unwrap(),
+            UserId::try_from("@registrar:matrix.org").unwrap(),
+            MatrixEventMock {
+                user_id: UserId::try_from("@alice:matrix.org").unwrap(),
+                message: hex::encode(invalid_signature.to_bytes()),
+            },
+        );
+
+        matrix.trigger_matrix_emitter(
+            RoomId::try_from("!1234:matrix.org").unwrap(),
+            UserId::try_from("@registrar:matrix.org").unwrap(),
+            MatrixEventMock {
+                user_id: UserId::try_from("@alice:matrix.org").unwrap(),
+                message: hex::encode(valid_signature.to_bytes()),
+            },
+        );
+
+        pause().await;
+
+        // Verify events.
+        let events = manager.events().await;
+        assert_eq!(events.len(), 11);
+
+        // Skip startup events...
+
+        assert_eq!(
+            events[4],
+            Event::Matrix(MatrixEvent::CreateRoom {
+                to_invite: UserId::try_from("@alice:matrix.org").unwrap(),
+            })
+        );
+
+        match &events[5] {
+            Event::Matrix(e) => match e {
+                MatrixEvent::SendMessage { room_id, message } => match message {
+                    VerifierMessage::InitMessageWithContext(_) => {}
+                    _ => panic!(),
+                },
+                _ => panic!(),
+            },
+            _ => panic!(),
+        }
+
+        match &events[6] {
+            Event::Matrix(e) => match e {
+                MatrixEvent::SendMessage { room_id, message } => match message {
+                    VerifierMessage::ResponseInvalid(_) => {}
+                    _ => panic!(),
+                },
+                _ => panic!(),
+            },
+            _ => panic!(),
+        }
+
+        match &events[7] {
+            Event::Matrix(e) => match e {
+                MatrixEvent::SendMessage { room_id, message } => match message {
+                    VerifierMessage::ResponseValid(_) => {}
+                    _ => panic!(),
+                },
+                _ => panic!(),
+            },
+            _ => panic!(),
+        }
+
+        match &events[8] {
+            Event::Connector(e) => match e {
+                ConnectorEvent::Writer { message } => {
+                    match message.event {
+                        EventType::JudgementResult => {}
+                        _ => panic!(),
+                    }
+
+                    assert_eq!(
+                        serde_json::from_value::<JudgementResponse>(message.data.clone())
+                            .unwrap()
+                            .judgement,
+                        Judgement::Reasonable
+                    );
+                }
+                _ => panic!(),
+            },
+            _ => panic!(),
+        }
+
+        match &events[9] {
+            Event::Matrix(e) => match e {
+                MatrixEvent::SendMessage { room_id, message } => match message {
+                    VerifierMessage::Goodbye(_) => {}
+                    _ => panic!(),
+                },
+                _ => panic!(),
+            },
+            _ => panic!(),
+        }
+
+        match &events[10] {
             Event::Matrix(e) => match e {
                 MatrixEvent::LeaveRoom { room_id } => {}
                 _ => panic!(),
