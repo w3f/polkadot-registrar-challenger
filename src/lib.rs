c4e34f3d998d730cb696d8db7b20438d633f0e86
lamafab <42901763+lamafab@users.noreply.github.com>
2021-01-17 03:29:13 +0100

    implement user permission types

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,10 @@ extern crate serde;
 #[macro_use]
 extern crate actix_web;
 
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::dev::RequestHead;
+use actix_web::guard::Guard;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder, Scope};
+use std::collections::{HashMap, HashSet};
 use std::env;
 use std::fs;
 
@@ -90,25 +93,105 @@ pub fn init_env() -> Result<Config> {
     Ok(config)
 }
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
 #[get("/healthcheck")]
 async fn healthcheck() -> impl Responder {
     HttpResponse::Ok().body("Ok")
+}
+
+#[get("/judge_identity")]
+async fn judge_identity() -> impl Responder {
+    HttpResponse::Ok().body("")
+}
+
+#[get("/status")]
+async fn status() -> impl Responder {
+    HttpResponse::Ok().body("")
+}
+
+fn admin_scope() -> Scope {
+    web::scope("/admin")
+        .guard(VerifyAdmin::default())
+        .service(judge_identity)
+}
+
+fn public_scope() -> Scope {
+    web::scope("/public").service(status)
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Hash)]
+pub struct UserId;
+
+pub struct UserPermissions {
+    permissions: HashSet<Permission>,
+}
+
+impl UserPermissions {
+    fn root_profile() -> Self {
+        UserPermissions {
+            permissions: vec![
+                Permission::AddUser,
+                Permission::RemoveUser,
+                Permission::JudgeIdentity,
+            ]
+            .into_iter()
+            .collect(),
+        }
+    }
+    fn judge_profile() -> Self {
+        UserPermissions {
+            permissions: vec![Permission::JudgeIdentity].into_iter().collect(),
+        }
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Hash)]
+pub enum Permission {
+    AddUser,
+    RemoveUser,
+    JudgeIdentity,
+}
+
+#[derive(Default)]
+struct VerifyAdmin {
+    users: HashMap<UserId, UserPermissions>,
+    check_for: Vec<Permission>,
+}
+
+impl VerifyAdmin {
+    fn check_for(check_for: Vec<Permission>) -> Self {
+        VerifyAdmin {
+            users: HashMap::default(),
+            check_for: check_for,
+        }
+    }
+}
+
+impl Guard for VerifyAdmin {
+    fn check(&self, _request: &RequestHead) -> bool {
+        // TODO: pull UUID from request.
+        let user_id = UserId;
+
+        if let Some(permissions) = self.users.get(&user_id) {
+            for permission in &self.check_for {
+                if !permissions.permissions.contains(permission) {
+                    return false;
+                }
+            }
+
+            true
+        } else {
+            false
+        }
+    }
 }
 
 #[rustfmt::skip]
 pub async fn run(_config: Config) -> Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(
-                web::scope("/admin")
-                    .service(hello)
-            )
             .service(healthcheck)
+            .service(admin_scope())
+            .service(public_scope())
         })
         .bind("127.0.0.1:8080")?
         .run()
