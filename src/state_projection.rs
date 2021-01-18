f32691dafb119a20a0f12b6e98fde4a22fb259de
lamafab <42901763+lamafab@users.noreply.github.com>
2021-01-18 12:24:22 +0100

    insert_identity method for IdentityState

--- src/state_projection.rs
+++ src/state_projection.rs
@@ -1,11 +1,30 @@
 use std::collections::HashMap;
+use std::sync::Arc;
+use tokio::sync::RwLock;
 
-pub struct StateProjection {}
+pub type IdentityStateProjection<'a> = Arc<RwLock<IdentityState<'a>>>;
 
-impl StateProjection {
+pub struct IdentityState<'a> {
+    identities: HashMap<IdentityAddress, Identity>,
+    identity_fields: Vec<&'a Vec<FieldStatus>>,
+}
+
+impl<'a> IdentityState<'a> {
     pub fn new() -> Self {
-        StateProjection {}
+        IdentityState {
+            identities: HashMap::new(),
+            identity_fields: vec![],
+        }
     }
+    pub fn insert_identity(&'a mut self, identity: Identity) {
+        let address = identity.address.clone();
+
+        self.identities.insert(identity.address.clone(), identity);
+        self.identity_fields
+            // Unwrapping here is safe since the entry was just inserted
+            .push(&self.identities.get(&address).unwrap().fields);
+    }
+    pub fn get_valid_verifications(&self, field: &IdentityField) {}
 }
 
 #[derive(Eq, PartialEq, Hash, Clone, Debug)]
@@ -32,29 +51,28 @@ pub struct FieldStatus {
 }
 
 #[derive(Eq, PartialEq, Hash, Clone, Debug, Serialize, Deserialize)]
+pub struct FieldAddress(String);
+
+#[derive(Eq, PartialEq, Hash, Clone, Debug, Serialize, Deserialize)]
 pub enum IdentityField {
     #[serde(rename = "legal_name")]
-    LegalName(),
+    LegalName(FieldAddress),
     #[serde(rename = "display_name")]
-    DisplayName,
+    DisplayName(FieldAddress),
     #[serde(rename = "email")]
-    Email,
+    Email(FieldAddress),
     #[serde(rename = "web")]
-    Web,
+    Web(FieldAddress),
     #[serde(rename = "twitter")]
-    Twitter,
+    Twitter(FieldAddress),
     #[serde(rename = "matrix")]
-    Matrix,
+    Matrix(FieldAddress),
     #[serde(rename = "pgpFingerprint")]
-    PGPFingerprint,
+    PGPFingerprint(FieldAddress),
     #[serde(rename = "image")]
+    /// NOTE: Currently unsupported.
     Image,
     #[serde(rename = "additional")]
+    /// NOTE: Currently unsupported.
     Additional,
-    // Reserved types for internal communication.
-    //
-    // Websocket connection to Watcher
-    ReservedConnector,
-    // Matrix emitter which reacts on Matrix messages
-    ReservedEmitter,
 }
