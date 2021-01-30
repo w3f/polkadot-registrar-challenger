ec61439d90efa306a5d61fc3a5ed4f94ddd7e302
lamafab <42901763+lamafab@users.noreply.github.com>
2021-01-30 23:12:11 +0100

    handle multiple types in session notifier

--- src/projection/identity_change_notifier.rs
+++ src/projection/identity_change_notifier.rs
@@ -1,6 +1,6 @@
-use crate::aggregate::verifier::VerifierAggregateId;
+use crate::aggregate::verifier::{self, VerifierAggregateId};
 use crate::api::ConnectionPool;
-use crate::event::{Event, StateWrapper};
+use crate::event::{Event, EventType, StateWrapper};
 use crate::Result;
 use eventually::store::Persisted;
 use eventually::Projection;
@@ -19,11 +19,18 @@ impl Projection for SessionNotifier {
     fn project(&mut self, event: Persisted<Self::SourceId, Self::Event>) -> BoxFuture<Result<()>> {
         let fut = async move {
             let event = event.take();
-            let body = event.expect_field_status_verified_ref()?;
+            let net_address = match event.body {
+                EventType::FieldStatusVerified(ref field_status) => &field_status.net_address,
+                EventType::IdentityFullyVerified(ref verified) => &verified.net_address,
+                _ => {
+                    warn!("Received unrecognized event when notifying sessions");
+                    return Ok(());
+                }
+            };
 
-            if let Some(notifier) = self.connection_pool.notify_net_address(&body.net_address) {
+            if let Some(notifier) = self.connection_pool.notify_net_address(net_address) {
                 if let Err(_) = notifier.send(event).await {
-                    error!("All connections to RPC sessions are closed");
+                    info!("All connections to RPC sessions are closed");
                 }
             }
 
