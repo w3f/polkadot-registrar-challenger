4eefeafd50f5e71382cdae585cf7c910b021625e
lamafab <42901763+lamafab@users.noreply.github.com>
2021-02-03 18:48:04 +0100

    init ws connection to remote

--- src/account_fetch.rs
+++ src/account_fetch.rs
@@ -1,7 +1,37 @@
 use crate::manager::{IdentityState, NetworkAddress};
 use crate::Result;
-use std::sync::Weak;
+use jsonrpc_client_transports::{TypedClient, RawClient};
+use jsonrpc_client_transports::transports::ws::connect as ws_connect;
+use jsonrpc_core::{MetaIoHandler, Params, Result as RpcResult, Value};
+use jsonrpc_derive::rpc;
+use std::str::FromStr;
 
 pub trait AccountFetch {
     fn fetch_account_state(net_address: &NetworkAddress) -> Result<Option<IdentityState>>;
+}
+
+#[rpc]
+pub trait RpcClient {
+    #[rpc(name = "protocolVersion")]
+    fn state_get_metadata(&self, s: Option<()>) -> RpcResult<String>;
+}
+
+pub struct SubstrateRpc {}
+
+impl SubstrateRpc {
+    async fn fetch_metadata() -> Result<()> {
+        let url = "wss://registrar-test-0.w3f.tech";
+
+        let client = ws_connect::<TypedClient>(&FromStr::from_str(url)?)
+            .await
+            .map_err(|_| anyhow!("Failed to connect to Substrate RPC"))?;
+
+        Ok(())
+    }
+}
+
+impl AccountFetch for SubstrateRpc {
+    fn fetch_account_state(net_address: &NetworkAddress) -> Result<Option<IdentityState>> {
+        unimplemented!()
+    }
 }
