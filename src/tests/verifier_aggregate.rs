6e11e0800f8d8a52da2a628b7e8e8ae3389195ab
lamafab <42901763+lamafab@users.noreply.github.com>
2021-02-08 20:13:58 +0100

    test verify_message_valid_multiple

--- src/tests/verifier_aggregate.rs
+++ src/tests/verifier_aggregate.rs
@@ -182,15 +182,6 @@ async fn verify_message_valid() {
     let alice = IdentityState::alice();
     let bob = IdentityState::bob();
 
-    let expected_message = alice
-        .fields
-        .get(&IdentityFieldType::Matrix)
-        .map(|field| match field.challenge() {
-            ChallengeStatus::ExpectMessage(challenge) => challenge.expected_message.clone(),
-            _ => panic!(),
-        })
-        .unwrap();
-
     // Execute commands.
     let mut root = repo.get(VerifierAggregateId).await.unwrap();
     root.handle(VerifierCommand::InsertIdentity(alice.clone()))
@@ -205,6 +196,15 @@ async fn verify_message_valid() {
     repo.add(root).await.unwrap();
 
     // Prepare message.
+    let expected_message = alice
+        .fields
+        .get(&IdentityFieldType::Matrix)
+        .map(|field| match field.challenge() {
+            ChallengeStatus::ExpectMessage(challenge) => challenge.expected_message.clone(),
+            _ => panic!(),
+        })
+        .unwrap();
+
     let message = ExternalMessage {
         origin: ExternalOrigin::Matrix,
         field_address: FieldAddress::from("@alice:matrix.org".to_string()),
@@ -299,8 +299,7 @@ async fn verify_message_invalid() {
     repo.add(root).await.unwrap();
 
     // Set the expected state.
-    let mut alice_new = alice.clone();
-    let current_state = alice_new
+    let current_state = alice
         .fields
         .get(&IdentityFieldType::Matrix)
         .unwrap()
@@ -328,4 +327,134 @@ async fn verify_message_invalid() {
     let state = root.state();
     assert!(state.contains(&alice));
     assert!(state.contains(&bob));
+}
+
+#[tokio::test]
+async fn verify_message_valid_multiple() {
+    let be = InMemBackend::<VerifierAggregateId>::run().await;
+    let store = be.store();
+    let mut repo = Repository::new(VerifierAggregate.into(), store);
+
+    let alice = IdentityState::alice();
+    let bob = IdentityState::bob();
+
+    // Execute commands.
+    let mut root = repo.get(VerifierAggregateId).await.unwrap();
+    root.handle(VerifierCommand::InsertIdentity(alice.clone()))
+        .await
+        .unwrap();
+
+    root.handle(VerifierCommand::InsertIdentity(bob.clone()))
+        .await
+        .unwrap();
+
+    // Commit changes
+    repo.add(root).await.unwrap();
+
+    // Prepare message.
+    let expected_message = alice
+        .fields
+        .get(&IdentityFieldType::Matrix)
+        .map(|field| match field.challenge() {
+            ChallengeStatus::ExpectMessage(challenge) => challenge.expected_message.clone(),
+            _ => panic!(),
+        })
+        .unwrap();
+
+    let messages = vec![
+        // Invalid
+        ExternalMessage {
+            origin: ExternalOrigin::Matrix,
+            field_address: FieldAddress::from("@alice:matrix.org".to_string()),
+            message: ProvidedMessage::from(ExpectedMessage::gen()),
+        },
+        // Invalid
+        ExternalMessage {
+            origin: ExternalOrigin::Matrix,
+            field_address: FieldAddress::from("@alice:matrix.org".to_string()),
+            message: ProvidedMessage::from(ExpectedMessage::gen()),
+        },
+        // Valid
+        ExternalMessage {
+            origin: ExternalOrigin::Matrix,
+            field_address: FieldAddress::from("@alice:matrix.org".to_string()),
+            message: ProvidedMessage::from(expected_message.clone()),
+        },
+        // Valid second time
+        ExternalMessage {
+            origin: ExternalOrigin::Matrix,
+            field_address: FieldAddress::from("@alice:matrix.org".to_string()),
+            message: ProvidedMessage::from(expected_message),
+        },
+        // Invalid
+        ExternalMessage {
+            origin: ExternalOrigin::Matrix,
+            field_address: FieldAddress::from("@alice:matrix.org".to_string()),
+            message: ProvidedMessage::from(ExpectedMessage::gen()),
+        },
+    ];
+
+    // Execute commands.
+    let mut root = repo.get(VerifierAggregateId).await.unwrap();
+    for message in messages {
+        root.handle(VerifierCommand::VerifyMessage(message))
+            .await
+            .unwrap();
+    }
+
+    // Commit changes
+    repo.add(root).await.unwrap();
+
+    // Set the expected state.
+    let current_state = alice
+        .fields
+        .get(&IdentityFieldType::Matrix)
+        .unwrap()
+        .clone();
+
+    let mut alice_new = alice.clone();
+    let new_field_state = alice_new
+        .fields
+        .get_mut(&IdentityFieldType::Matrix)
+        .map(|status| {
+            match status.challenge_mut() {
+                ChallengeStatus::ExpectMessage(challenge) => challenge.status = Validity::Valid,
+                _ => panic!(),
+            }
+
+            status.clone()
+        })
+        .unwrap();
+
+    // Check the resulting events.
+    let expected = [
+        Event::from(EventType::IdentityInserted(alice.clone().into())),
+        Event::from(EventType::IdentityInserted(bob.clone().into())),
+        Event::from(EventType::FieldStatusVerified(FieldStatusVerified {
+            net_address: alice.net_address.clone(),
+            field_status: current_state.clone(),
+        })),
+        Event::from(EventType::FieldStatusVerified(FieldStatusVerified {
+            net_address: alice.net_address.clone(),
+            field_status: current_state,
+        })),
+        Event::from(EventType::FieldStatusVerified(FieldStatusVerified {
+            net_address: alice.net_address.clone(),
+            field_status: new_field_state,
+        })),
+    ];
+
+    let events = be.get_events(VerifierAggregateId).await;
+    assert_eq!(events.len(), expected.len());
+
+    for (expected, event) in expected.iter().zip(events.iter()) {
+        assert_eq!(expected.body, event.body);
+    }
+
+    // Check the resulting state.
+    let root = repo.get(VerifierAggregateId).await.unwrap();
+    let state = root.state();
+    assert!(!state.contains(&alice));
+    assert!(state.contains(&bob));
+    assert!(state.contains(&alice_new));
 }
