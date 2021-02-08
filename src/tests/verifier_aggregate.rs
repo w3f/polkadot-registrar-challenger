5f1fe9112e1fefeba4d526937e3e6c5adcc9df7d
lamafab <42901763+lamafab@users.noreply.github.com>
2021-02-08 16:24:58 +0100

    track verifier_aggregate test module

---
+++ src/tests/verifier_aggregate.rs
@@ -0,0 +1,53 @@
+use super::InMemBackend;
+use crate::aggregate::verifier::{VerifierAggregate, VerifierAggregateId, VerifierCommand};
+use crate::event::{Event, EventType};
+use crate::manager::IdentityState;
+use eventually::{Repository, Subscription};
+use futures::StreamExt;
+
+#[tokio::test]
+async fn insert_identities() {
+    let be = InMemBackend::<VerifierAggregateId>::run().await;
+    let store = be.store();
+    let mut repo = Repository::new(VerifierAggregate.into(), store);
+
+    let alice = IdentityState::alice();
+    let bob = IdentityState::bob();
+
+    let mut root = repo.get(VerifierAggregateId).await.unwrap();
+
+    // Execute commands.
+    root.handle(VerifierCommand::InsertIdentity(alice.clone()))
+        .await
+        .unwrap();
+
+    root.handle(VerifierCommand::InsertIdentity(bob.clone()))
+        .await
+        .unwrap();
+
+    // Commit changes
+    repo.add(root).await.unwrap();
+
+    // Check the resulting events.
+    let expected = [
+        Event::from(EventType::IdentityInserted(alice.clone().into())),
+        Event::from(EventType::IdentityInserted(bob.clone().into())),
+    ];
+
+    let events = be.take_events(VerifierAggregateId, expected.len()).await;
+    for (expected, event) in expected.iter().zip(events.iter()) {
+        assert_eq!(expected.body, event.body);
+    }
+
+    // Check the resulting state.
+    let root = repo.get(VerifierAggregateId).await.unwrap();
+    let state = root.state();
+    assert!(state.contains(&alice));
+    assert!(state.contains(&bob));
+}
+
+#[tokio::test]
+async fn second() {
+    let be = InMemBackend::<VerifierAggregateId>::run().await;
+    let store = be.store();
+}
