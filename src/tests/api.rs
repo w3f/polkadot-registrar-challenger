82cb43df1faad33da10e624d4b885786b80ea98a
lamafab <42901763+lamafab@users.noreply.github.com>
2021-02-11 22:50:26 +0100

    extend yielding functionality

--- src/tests/api.rs
+++ src/tests/api.rs
@@ -5,6 +5,7 @@ use crate::manager::IdentityState;
 use eventually::Repository;
 use futures::StreamExt;
 use jsonrpc_core::types::{to_value, Params, Value};
+use matrix_sdk::api::r0::account::request_registration_token_via_email;
 use std::sync::{Arc, Condvar, Mutex};
 use std::time::Duration;
 
@@ -13,37 +14,47 @@ struct Regulator<T> {
 }
 
 struct RegulatorLock<T> {
-    lock: Mutex<T>,
+    lock: Mutex<Option<T>>,
     peers: Condvar,
 }
 
 struct RegulatorChild<T> {
-    my_turn: T,
+    my_turn: Option<T>,
     inner: Arc<RegulatorLock<T>>,
 }
 
-impl<T: Default> Regulator<T> {
+impl<T> Regulator<T> {
     fn new() -> Self {
         Regulator {
             inner: Arc::new(RegulatorLock {
-                lock: Mutex::new(T::default()),
+                lock: Mutex::new(None),
                 peers: Condvar::new(),
             }),
         }
     }
     fn new_child(&self, my_turn: T) -> RegulatorChild<T> {
         RegulatorChild {
-            my_turn: my_turn,
+            my_turn: Some(my_turn),
             inner: Arc::clone(&self.inner),
         }
     }
 }
 
-impl<T: PartialEq> RegulatorChild<T> {
+impl<T: PartialEq + std::fmt::Debug> RegulatorChild<T> {
     fn yield_time_to(&self, to: T) {
+        self.yield_and_exit(to);
+        self.wait()
+    }
+    fn yield_and_exit(&self, to: T) {
         let mut round = self.inner.lock.lock().unwrap();
-        *round = to;
+        *round = Some(to);
+
+        drop(round);
+
         self.inner.peers.notify_all();
+    }
+    fn wait(&self) {
+        let mut round = self.inner.lock.lock().unwrap();
         while *round != self.my_turn {
             // Wait until it's my round again
             round = self.inner.peers.wait(round).unwrap();
@@ -51,7 +62,7 @@ impl<T: PartialEq> RegulatorChild<T> {
     }
 }
 
-#[derive(PartialEq)]
+#[derive(Debug, PartialEq)]
 enum Thread {
     Aggregate,
     Stream,
@@ -107,14 +118,16 @@ fn subscribe_status_pending_judgement_request() {
         let api = ApiBackend::run().await;
         let alice = IdentityState::alice();
 
+        reg_stream.wait();
+
         #[rustfmt::skip]
         let expected = [
             to_value(ErrorMessage::no_pending_judgement_request(0)).unwrap(),
         ];
 
         // No pending judgement request is available at first.
-        let messages = api
-            .get_messages(
+        let mut stream = api.client()
+            .subscribe(
                 "account_subscribeStatus",
                 Params::Array(vec![
                     Value::String(alice.net_address.net_str().to_string()),
@@ -122,18 +135,17 @@ fn subscribe_status_pending_judgement_request() {
                 ]),
                 "account_status",
                 "account_unsubscribeStatus",
-            )
-            .await;
+            ).unwrap();
 
-        // Tell the test to continue with the insertion of the identity.
-        reg_stream.yield_time_to(Thread::Aggregate);
+        let mut counter = 0;
+        while let Some(message) = stream.next().await {
+            //assert_eq!(message.unwrap(), expected[counter]);
+            println!(">>> {:?}", message.unwrap());
 
-        assert_eq!(messages.len(), expected.len());
-        /*
-        for (message, expected) in messages.iter().zip(expected.iter()) {
-            assert_eq!(message, expected)
+            reg_stream.yield_and_exit(Thread::Aggregate);
+
+            counter += 1;
         }
-        */
     });
 
     reg_aggr.yield_time_to(Thread::Stream);
