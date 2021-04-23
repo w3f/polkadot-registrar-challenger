673cda169a1c880e7c07d81f9ff2147527f15f0c
lamafab <42901763+lamafab@users.noreply.github.com>
2021-04-23 19:46:42 +0200

    fetch unread messages

--- src/database.rs
+++ src/database.rs
@@ -1,5 +1,27 @@
+use crate::primitives::{IdentityContext, ChainAddress, ChainRemark, ExternalMessage, JudgementState, Timestamp, MessageId};
 use crate::Result;
-use crate::primitives::{ChainAddress, ChainRemark, JudgementState, ExternalMessage};
+use bson::{doc, from_document, to_bson, to_document, Bson, Document};
+use mongodb::{options::UpdateOptions, Client, Database as MongoDb};
+use serde::Serialize;
+use futures::StreamExt;
+use std::collections::HashSet;
+
+const MESSAGE_COLLECTION: &'static str = "external_messages";
+
+/// Convenience trait. Converts a value to BSON.
+trait ToBson {
+    fn to_bson(&self) -> Result<Bson>;
+    fn to_document(&self) -> Result<Document>;
+}
+
+impl<T: Serialize> ToBson for T {
+    fn to_bson(&self) -> Result<Bson> {
+        Ok(to_bson(self)?)
+    }
+    fn to_document(&self) -> Result<Document> {
+        Ok(to_document(self)?)
+    }
+}
 
 pub enum VerifyOutcome {
     FieldOk,
@@ -7,28 +29,74 @@ pub enum VerifyOutcome {
 }
 
 pub struct Database {
-
+    db: MongoDb,
+    last_message_check: Timestamp,
 }
 
 impl Database {
-    pub fn new() -> Self {
-        Database {
-
-        }
+    pub async fn new(uri: &str, db: &str) -> Result<Self> {
+        Ok(Database {
+            db: Client::with_uri_str(uri).await?.database(db),
+            last_message_check: Timestamp::now(),
+        })
     }
     pub fn add_judgement_request(&self, request: JudgementState) -> Result<()> {
         unimplemented!()
     }
-    pub fn add_message(&self) -> Result<()> {
+    pub async fn add_message(&self, message: ExternalMessage) -> Result<()> {
+        let coll = self.db.collection(MESSAGE_COLLECTION);
+
+        coll.update_one(
+            doc! {
+                "type": message.ty.to_bson()?,
+                "id": message.id.to_bson()?,
+            },
+            message.to_document()?,
+            Some({
+                let mut options = UpdateOptions::default();
+                options.upsert = Some(true);
+                options
+            }),
+        )
+        .await?;
+
+        Ok(())
+    }
+    pub async fn add_chain_remark(&self, remark: ChainRemark) -> Result<()> {
         unimplemented!()
     }
-    pub fn add_chain_remark(&self, remark: ChainRemark) -> Result<()> {
+    pub async fn fetch_messages(&self) -> Result<Vec<ExternalMessage>> {
+        let coll = self.db.collection(MESSAGE_COLLECTION);
+
+        // Read the latest, unprocessed messages.
+        let mut cursor = coll.find(
+            doc! {
+                "timestamp": {
+                    "$gt": self.last_message_check.to_bson()?,
+                }
+            },
+            None,
+        )
+        .await?;
+
+        // Parse and collect messages.
+        let mut messages = vec![];
+        while let Some(doc) = cursor.next().await {
+            messages.push(from_document(doc?)?);
+        }
+
+        Ok(messages)
+    }
+    pub async fn verify_message(&self, message: ExternalMessage) -> Result<VerifyOutcome> {
         unimplemented!()
     }
-    pub fn verify_message(&self, message: ExternalMessage) -> Result<VerifyOutcome> {
+    pub async fn set_validity() -> Result<()> {
         unimplemented!()
     }
-    pub fn check_state_change(&self, chain_address: ChainAddress) -> Result<JudgementState> {
+    pub async fn fetch_judgement_state(
+        &self,
+        chain_address: IdentityContext,
+    ) -> Result<JudgementState> {
         unimplemented!()
     }
 }
--- src/primitives.rs
+++ src/primitives.rs
@@ -1,20 +1,29 @@
 #[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all="snake_case")]
+#[serde(rename_all = "snake_case")]
+pub struct IdentityContext {
+    pub chain_address: ChainAddress,
+    pub chain_name: ChainName,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
 pub struct ChainAddress(String);
 
 #[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all="snake_case")]
+#[serde(rename_all = "snake_case")]
 pub enum ChainName {
     Polkadot,
     Kusama,
 }
 
 #[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all="snake_case")]
-pub struct ChainRemark(String);
+#[serde(rename_all = "snake_case")]
+pub struct ChainRemark {
+    context: IdentityContext,
+}
 
 #[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all="snake_case")]
+#[serde(rename_all = "snake_case")]
 pub struct IdentityField {
     field: IdentityFieldType,
     is_verified: bool,
@@ -22,7 +31,7 @@ pub struct IdentityField {
 }
 
 #[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all="snake_case", tag="type", content="value")]
+#[serde(rename_all = "snake_case", tag = "type", content = "value")]
 pub enum IdentityFieldType {
     LegalName(String),
     DisplayName(String),
@@ -36,28 +45,25 @@ pub enum IdentityFieldType {
 }
 
 #[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all="snake_case")]
+#[serde(rename_all = "snake_case")]
 pub struct JudgementState {
-    pub chain_address: ChainAddress,
-    pub chain_name: ChainName,
+    pub context: IdentityContext,
     pub fields: Vec<IdentityField>,
 }
 
 #[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all="snake_case")]
+#[serde(rename_all = "snake_case")]
 pub struct ExternalMessage {
-    ty: ExternalMessageType,
-    id: u64,
-    origin: ExternalMessageOrigin,
+    #[serde(rename = "type")]
+    pub ty: ExternalMessageType,
+    pub id: MessageId,
+    pub timestamp: Timestamp,
+    pub origin: ExternalMessageOrigin,
     values: Vec<MessagePart>,
 }
 
 #[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all="snake_case")]
-pub struct ExternalMessageOrigin(String);
-
-#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all="snake_case")]
+#[serde(rename_all = "snake_case")]
 pub enum ExternalMessageType {
     Email,
     Twitter,
@@ -65,18 +71,37 @@ pub enum ExternalMessageType {
 }
 
 #[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all="snake_case")]
+#[serde(rename_all = "snake_case")]
+pub struct MessageId(u64);
+
+#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub struct Timestamp(u64);
+
+impl Timestamp {
+    pub fn now() -> Self {
+        Timestamp(0)
+    }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub struct ExternalMessageOrigin(String);
+
+#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
 pub struct MessagePart(String);
 
 #[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all="snake_case")]
+#[serde(rename_all = "snake_case")]
 pub struct Notification {
+    #[serde(rename = "type")]
     ty: NotificationType,
     message: NotificationMessage,
 }
 
 #[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all="snake_case")]
+#[serde(rename_all = "snake_case")]
 pub enum NotificationType {
     Success,
     Info,
@@ -85,8 +110,8 @@ pub enum NotificationType {
 }
 
 #[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all="snake_case")]
+#[serde(rename_all = "snake_case")]
 pub enum NotificationMessage {
     NoJudgementRequest,
-    FieldVerified(IdentityFieldType)
+    FieldVerified(IdentityFieldType),
 }
