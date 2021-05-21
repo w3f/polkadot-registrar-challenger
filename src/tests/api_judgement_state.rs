8b68a3d3a293617ed5890e0e0d37108ec6e593ed
lamafab <42901763+lamafab@users.noreply.github.com>
2021-05-21 03:23:19 +0200

    test for empty stream

--- src/tests/api_judgement_state.rs
+++ src/tests/api_judgement_state.rs
@@ -1,7 +1,7 @@
-use super::{new_env, ToWsMessage};
+use super::*;
 use crate::database::Database;
 use crate::primitives::{IdentityContext, JudgementState};
-use futures::{SinkExt, StreamExt};
+use futures::{SinkExt, StreamExt, FutureExt};
 
 #[actix::test]
 async fn add_identity() {
@@ -14,4 +14,7 @@ async fn add_identity() {
     stream.send(IdentityContext::alice().to_ws()).await.unwrap();
     let x = stream.next().await;
     println!("{:?}", x);
+
+    wait().await;
+    assert!(stream.next().now_or_never().is_none());
 }
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -4,8 +4,11 @@ use crate::adapters::AdapterListener;
 use crate::database::Database;
 use actix_test::TestServer;
 use actix_web_actors::ws::Message;
+use futures::FutureExt;
 use rand::{thread_rng, Rng};
 use serde::Serialize;
+use tokio::time::{sleep, Duration};
+use std::future::Future;
 
 mod api_judgement_state;
 
@@ -17,6 +20,10 @@ impl<T: Serialize> ToWsMessage for T {
     fn to_ws(&self) -> Message {
         Message::Text(serde_json::to_string(&self).unwrap().into())
     }
+}
+
+async fn wait() {
+    sleep(Duration::from_secs(1)).await;
 }
 
 async fn new_env() -> (Database, TestServer, MessageInjector) {
