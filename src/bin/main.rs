5b93a0fa2c7453e658e6e3ab2db03294136ccde3
lamafab <42901763+lamafab@users.noreply.github.com>
2021-06-06 00:49:13 +0200

    move instance logic to lib

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,51 +1,11 @@
 #[macro_use]
 extern crate log;
-#[macro_use]
-extern crate anyhow;
 
-use futures::{select, FutureExt};
-use libregistrar::{
-    init_env, run_adapters_blocking, run_rest_api_server_blocking, AdapterConfig, Database,
-    InstanceType, NotifierConfig, Result, SessionNotifier,
-};
-
-async fn config_adapter_listener(config: AdapterConfig) -> Result<()> {
-    let db = Database::new(&config.db.uri, &config.db.db_name).await?;
-    run_adapters_blocking(config.accounts, db).await
-}
-
-async fn config_session_notifier(config: NotifierConfig) -> Result<()> {
-    let db = Database::new(&config.db.uri, &config.db.db_name).await?;
-    let server = run_rest_api_server_blocking(&config.api_address, db.clone()).await?;
-    Ok(SessionNotifier::new(db, server).run_blocking().await)
-}
+use libregistrar::{run, Result};
 
 #[actix::main]
 async fn main() -> Result<()> {
-    let config = init_env()?;
-
-    match config.instance {
-        InstanceType::AdapterListener(config) => {
-            config_adapter_listener(config).await?;
-        }
-        InstanceType::SessionNotifier(config) => {
-            config_session_notifier(config).await?;
-        }
-        InstanceType::SingleInstance(config) => {
-            let (adapter, notifier) = (config.adapter, config.notifier);
-
-            let a = tokio::spawn(async move { config_adapter_listener(adapter).await });
-            let b = tokio::spawn(async move { config_session_notifier(notifier).await });
-
-            // If one thread exits, exit the full application.
-            select! {
-                res = a.fuse() => res??,
-                res = b.fuse() => res??,
-            }
-        }
-    }
-
+    run().await?;
     error!("Service exited unexpectedly");
-
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@ extern crate async_trait;
 use log::LevelFilter;
 use std::env;
 use std::fs;
+use futures::{select, FutureExt};
 
 pub type Result<T> = std::result::Result<T, anyhow::Error>;
 
@@ -155,4 +156,42 @@ pub fn init_env() -> Result<Config> {
     println!("Logger initiated");
 
     Ok(config)
+}
+
+pub async fn run() -> Result<()> {
+    async fn config_adapter_listener(config: AdapterConfig) -> Result<()> {
+        let db = Database::new(&config.db.uri, &config.db.db_name).await?;
+        run_adapters_blocking(config.accounts, db).await
+    }
+
+    async fn config_session_notifier(config: NotifierConfig) -> Result<()> {
+        let db = Database::new(&config.db.uri, &config.db.db_name).await?;
+        let server = run_rest_api_server_blocking(&config.api_address, db.clone()).await?;
+        Ok(SessionNotifier::new(db, server).run_blocking().await)
+    }
+
+    let config = init_env()?;
+
+    match config.instance {
+        InstanceType::AdapterListener(config) => {
+            config_adapter_listener(config).await?;
+        }
+        InstanceType::SessionNotifier(config) => {
+            config_session_notifier(config).await?;
+        }
+        InstanceType::SingleInstance(config) => {
+            let (adapter, notifier) = (config.adapter, config.notifier);
+
+            let a = tokio::spawn(async move { config_adapter_listener(adapter).await });
+            let b = tokio::spawn(async move { config_session_notifier(notifier).await });
+
+            // If one thread exits, exit the full application.
+            select! {
+                res = a.fuse() => res??,
+                res = b.fuse() => res??,
+            }
+        }
+    }
+
+    Ok(())
 }
