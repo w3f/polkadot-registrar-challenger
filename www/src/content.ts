4324bd959a35e3e1e2976c2a43c5511e553eee2d
lamafab <42901763+lamafab@users.noreply.github.com>
2021-07-24 16:25:22 +0200

    correct ids in inner html

--- www/src/content.ts
+++ www/src/content.ts
@@ -2,31 +2,15 @@ import { State } from './json';
 
 export class ContentManager {
 	div_display_name_overview: HTMLElement;
-	display_name_value: HTMLElement;
-	display_name_validity: HTMLElement;
     div_fully_verified_info: HTMLElement;
-
 	div_verification_overview: HTMLElement;
-	verification_overview: HTMLElement;
-
 	div_email_second_challenge: HTMLElement;
-	email_second_challenge: HTMLElement;
-
 	div_unsupported_overview: HTMLElement;
-	unsupported_overview: HTMLElement;
 
 	constructor() {
 		this.div_display_name_overview =
 			document
 				.getElementById("div-display-name-overview")!;
-
-		this.display_name_value =
-			document
-				.getElementById("display-name-value")!;
-
-		this.display_name_validity =
-			document
-				.getElementById("display-name-validity")!;
 
         this.div_fully_verified_info =
             document
@@ -36,43 +20,19 @@ export class ContentManager {
 			document
 				.getElementById("div-verification-overview")!;
 
-		this.verification_overview =
-			document
-				.getElementById("verification-overview")!;
-
 		this.div_email_second_challenge =
 			document
 				.getElementById("div-email-second-challenge")!;
 
-		this.email_second_challenge =
-			document
-				.getElementById("email-second-challenge")!;
-
 		this.div_unsupported_overview =
 			document
 				.getElementById("div-unsupported-overview")!;
-
-		this.unsupported_overview =
-			document
-				.getElementById("unsupported-overview")!;
 	}
 
 	processVerificationOverviewTable(state: State) {
         // TODO: Check if 'fields` is empty.
 
-        let table = `
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Type</th>
-                    <th scope="col">Challenge</th>
-                    <th scope="col">From</th>
-                    <th scope="col">To</th>
-                    <th scope="col">Status</th>
-                </tr>
-            </thead>
-            <tbody>
-        `;
+        let table = "";
 
         let counter = 1;
 		for (let field of state.fields) {
@@ -82,7 +42,7 @@ export class ContentManager {
 					if (field.challenge.content.second && !field.challenge.content.second!.is_verified) {
 						validity = BadgeVerifiedHalf;
 
-						this.email_second_challenge.innerHTML = `${field.value.value}`;
+                        this.setEmailSecondChallengeContent(field.value.value);
 					} else {
 						validity = BadgeVerified;
 
@@ -119,28 +79,25 @@ export class ContentManager {
 
 				counter += 1;
 			} else if (field.challenge.challenge_type == "background_check" && field.value.type == "display_name") {
-				let elem = this.display_name_validity;
+                let validity;
 
 				if (field.challenge.content.passed) {
-                    elem.innerHTML = BadgeValid;
+                    validity = BadgeValid;
 				} else {
-					elem.innerHTML = BadgeInvalid;
+					validity = BadgeInvalid;
 				}
 
-				this.display_name_value.innerHTML = field.value.value;
+                this.setDisplayNameVerification(field.value.value, validity)
 			}
 
-            table += '</tbody>';
-
             // Apply table to the page.
-            this.div_verification_overview.innerHTML = ContentVerificationOverview;
-            this.verification_overview.innerHTML = table;
+            this.setVerificationOverviewContent(table);
 
             // Display banner if fully verified.
             if (state.is_fully_verified) {
-                this.div_fully_verified_info.innerHTML = ContentFullyVerified;
+                this.setFullyVerifiedContent
             } else {
-                this.div_fully_verified_info.innerHTML = "";
+                this.wipeFullyVerifiedContent
             }
 		}
 	}
@@ -153,11 +110,94 @@ export class ContentManager {
         }
 
         if (unsupported.length != 0) {
-            this.div_unsupported_overview.innerHTML = ContentUnsupportedWarnings;
-            this.unsupported_overview.innerHTML = unsupported;
+            this.setUnsupportedContent(unsupported);
         } else {
-            this.div_unsupported_overview.innerHTML = "";
+            this.wipeUnsupportedContent;
         }
+    }
+    setFullyVerifiedContent() {
+        this.div_fully_verified_info.innerHTML = `
+            <div class="row justify-content-center">
+                <div class="col-10 table-responsive bg-success p-2">
+                    <h2 class="text-center text-white">Identity fully verified! ✔</h2>
+                </div>
+            </div>
+        `;
+    }
+    wipeFullyVerifiedContent() {
+        this.div_fully_verified_info.innerHTML = "";
+    }
+    setDisplayNameVerification(name: string, validity: string) {
+        this.div_display_name_overview.innerHTML = `
+            <div class="col-10 ">
+                <h2>Display name check</h2>
+                <p>The display name <strong>${name}</strong> is ${validity}</p>
+            </div>
+        `;
+    }
+    setVerificationOverviewContent(table: string) {
+        this.div_verification_overview.innerHTML = `
+            <div class="col-10 table-responsive ">
+                <h2>Account verification</h2>
+                <p>The service expects a message <strong>from</strong> the specified account in the on-chain
+                    identity sent <strong>to</strong> the corresponding W3F account containing the
+                    <strong>challenge</strong>. Do note that each account type has its own, individual
+                    challenge.
+                </p>
+                <table id="verification-overview" class="table table-striped table-dark">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Type</th>
+                            <th scope="col">Challenge</th>
+                            <th scope="col">From</th>
+                            <th scope="col">To</th>
+                            <th scope="col">Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        ${table}
+                    </tbody>
+                </table>
+            </div>
+        `;
+    }
+    setEmailSecondChallengeContent(address: string) {
+        this.div_email_second_challenge.innerHTML = `
+            <div class="col-10">
+                <h2>⚠️️ Additional Challenge</h2>
+                <p>An message containing an additional challenge was sent to <strong>
+                    ${address}</strong>. Please insert that challenge in the following field:
+                </p>
+                <div class="input-group">
+                <input type="text" class="form-control"
+                    aria-label="Second challenge verification" placeholder="Challenge...">
+                <button id="execute-second-challenge" class="col-1 btn btn-primary"
+                    type="button">Verify</button>
+                </div>
+            </div>
+        `;
+    }
+    setUnsupportedContent(list: string) {
+        this.div_unsupported_overview.innerHTML = `
+            <div class="col-10">
+                <h2>🚨 Unsupported entries</h2>
+                <ul>
+                    ${list}
+                </ul>
+                <p>The identity on-chain info contains fields that are not supported by the W3F registrar service in
+                an automated manner and <em>must</em> be removed. If you really want to have those fields
+                included, please contact the
+                appropriate authorities as described in the <em>"Need help?"</em> section below. Please prepare
+                the
+                appropriate information so the manual verification can be completed as quickly as possible. For
+                example, if you want to add a web address, please make sure that the website somehow references
+                your Kusama/Polkadot address.</p>
+            </div>
+        `;
+    }
+    wipeUnsupportedContent() {
+        this.div_unsupported_overview.innerHTML = "";
     }
 }
 
@@ -189,64 +229,5 @@ const BadgeInvalid = `
 const ContentLiveUpdates = `
 	<div class="col-10">
 		<p class="text-center"><em>Displaying live updates...</em></p>
-	</div>
-`;
-
-const ContentFullyVerified = `
-	<div class="row justify-content-center">
-		<div class="col-10 table-responsive bg-success p-2">
-			<h2 class="text-center text-white">Identity fully verified! ✔</h2>
-		</div>
-	</div>
-`;
-
-const ContentDisplayNameResult = `
-	<div class="col-10 ">
-		<h2>Display name check</h2>
-		<p>The display name <strong id="display-name-value"></strong> is <span
-			id="display-name-validity"></span></p>
-	</div>
-`;
-
-const ContentUnsupportedWarnings = `
-	<div class="col-10">
-		<h2>🚨 Unsupported entries</h2>
-		<ul id="unsupported-overview">
-		</ul>
-		<p>The identity on-chain info contains fields that are not supported by the W3F registrar service in
-		an automated manner and <em>must</em> be removed. If you really want to have those fields
-		included, please contact the
-		appropriate authorities as described in the <em>"Need help?"</em> section below. Please prepare
-		the
-		appropriate information so the manual verification can be completed as quickly as possible. For
-		example, if you want to add a web address, please make sure that the website somehow references
-		your Kusama/Polkadot address.</p>
-	</div>
-`;
-
-const ContentEmailSecondChallenge = `
-	<div class="col-10">
-		<h2>⚠️️ Additional Challenge</h2>
-		<p>An message containing an additional challenge was sent to <strong
-			id="email-second-challenge"></strong>. Please insert that challenge in the following field:
-		</p>
-		<div class="input-group">
-		<input id="specify-second-challenge" type="text" class="form-control"
-			aria-label="Second challenge verification" placeholder="Challenge...">
-		<button id="execute-second-challenge" class="col-1 btn btn-primary"
-			type="button">Verify</button>
-		</div>
-	</div>
-`;
-
-const ContentVerificationOverview = `
-	<div class="col-10 table-responsive ">
-		<h2>Account verification</h2>
-		<p>The service expects a message <strong>from</strong> the specified account in the on-chain
-		identity sent <strong>to</strong> the corresponding W3F account containing the
-		<strong>challenge</strong>. Do note that each account type has its own, individual
-		challenge.
-		</p>
-		<table id="verification-overview" class="table table-striped table-dark"></table>
 	</div>
 `;
