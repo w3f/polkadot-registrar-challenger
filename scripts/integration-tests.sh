e417589571c07eaa0c5ac2005be8bd0261cbd019
Alessio Onori <alessio.onori@gmail.com>
2021-09-23 17:57:35 +0200

    Update integration-tests.sh

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -7,17 +7,11 @@ source /scripts/bootstrap-helm.sh
 run_tests() {
     echo Running tests...
 
-    wait_pod_ready registrar-challenger
+    wait_pod_ready registrar-challenger-session-notifier
 }
 
-teardown() {
-    helm delete registrar-challenger
-}
 
 main(){
-    if [ -z "$KEEP_W3F_POLKADOT_CHALLENGER" ]; then
-        trap teardown EXIT
-    fi
 
     /scripts/build-helmfile.sh
 
