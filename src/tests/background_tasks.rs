27756fef24609eab67368a90493ed538ca2ba70b
lamafab <42901763+lamafab@users.noreply.github.com>
2022-04-25 22:01:38 +0200

    test background outgoing watcher messages

---
+++ src/tests/background_tasks.rs
@@ -0,0 +1,16 @@
+use super::*;
+use tokio::time::{sleep, Duration};
+
+#[actix::test]
+async fn background_outgoing_watcher_messages() {
+    let (_db, mut connector, _api, _inj) = new_env().await;
+
+    // Wait until enough messages have been sent to the Watcher (mocked).
+    sleep(Duration::from_secs(10)).await;
+
+    let (out, counter) = connector.outgoing();
+    assert!(counter.provide_judgement == 0);
+    assert!(counter.request_pending_judgements > 5);
+    assert!(counter.request_display_names > 5);
+    assert!(counter.ping == 0);
+}
